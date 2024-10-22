50603cae04d1118b4c961758e587f3c3ddf7a80d
kleudyvictor <kleudyvictor@gmail.com>
2024-10-22 19:27:06 -0400

    actualizacion

--- store_api/settings.py
+++ store_api/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'store',
     'rest_framework',
-     'corsheaders',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
